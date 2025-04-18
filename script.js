5f5bf1d7a6459cb064eca6cfdc5b7154eb4b49ff
Shuja06 <shujaalidhandu06@gmail.com>
2025-04-18 21:04:34 +0530

    Updated HTML, CSS, and JavaScript files

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const navMenu = document.getElementById("nav-menu");
 const navLink = document.querySelectorAll(".nav-link")
 const hamburger = document.getElementById("hamburger");
-const rmHam = document.getElementById("remove-hamburger");
 
 hamburger.addEventListener("click", () => {
-    const isOpen = navMenu.classList.contains("left-[0]");
+    navMenu.classList.toggle("left-[0]");
+    hamburger.classList.toggle('ri-close-large-fill');
+})
 
-    if (isOpen) {
-        // Menu is open — close it
-        navMenu.classList.remove("left-[0]");
-        navMenu.classList.add("left-[-100%]");
-        hamburger.classList.remove("ri-close-large-fill");
-        hamburger.classList.add("ri-menu-4-line");
-    } else {
-        // Menu is closed — open it
-        navMenu.classList.remove("left-[-100%]");
-        navMenu.classList.add("left-[0]");
-        hamburger.classList.remove("ri-menu-4-line");
-        hamburger.classList.add("ri-close-large-fill");
-    }
-});
-
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle("left-[0]");
+        hamburger.classList.toggle('ri-close-large-fill');
+    })
+})
