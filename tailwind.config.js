a7ec910242c16559ced3b7473966e69ab74a16cb
Shuja06 <shujaalidhandu06@gmail.com>
2025-04-19 19:49:08 +0530

    Added main contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,27 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     screens: {
-      sm: "340px",
+      sm: "380px",
       md: "540px",
       lg: "768px",
       xl: "1180px"
     },
 
     extend: {},
+
+    keyframes: {
+      move: {
+        "50%" : { transform : "translateY(-1.5rem)"}
+      },
+      rotate: {
+        "0%": {transform: "rotate(0deg)"},
+        "100%": {transform: "rotate(360deg)"},
+      }
+    },
+    animation: {
+      movingY: "move 3s linear infinite",
+      rotating: "rotate 15s linear infinite",
+    },
 
     fontFamily: {
       Jost: ["Jost", "sans-serif"],
@@ -19,7 +33,8 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        DEFAULT: "12PX",
+        DEFAULT: "12px",
+        sm: "12px",
         md: "32px"
       }
     }
